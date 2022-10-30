6879fd2e1f7102e81e7c4170811c018bfa0dea41
Tommaso Brumani <tommaso.brumani@libero.it>
2022-10-30 18:32:46 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,15 @@
+# Reinforcement Learning Exercises
+## Overview
+The repository contains `Python` imeplementations of various <b>reinforcement learning</b> models, realized for the most part using `pyTorch`.
+
+### License
+The exercises were carried out as assignments for the 2022/2023 '<b>Reinforcement Learning</b>' course at <b>Aalto University</b>. 
+
+The base infrastructure of the models and environments was implemented by the course staff and provided to the students with core pieces of code missing, which had to be filled-in accordingly.
+
+## Project Specifications
+Each of the exercises has distinct specifications, which are provided in the 'documentation' folder inside of each exercise folder.
+
+### File Systems Structure
+* `exN`: the folder containing the code and documentation for exercise 'N'
+* `common`: utility functions used throughout the exercises
---
+++ ex4/dqn_agent.py
@@ -0,0 +1,125 @@
+import sys, os
+sys.path.insert(0, os.path.abspath(".."))
+from typing import List
+import numpy as np
+import torch
+import torch.nn as nn
+import torch.optim as optim
+import torch.nn.functional as F
+import random
+import copy
+from common import helper as h
+
+device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+
+def mlp(in_dim, mlp_dims: List[int], out_dim, act_fn=nn.ReLU, out_act=nn.Identity):
+    """Returns an MLP."""
+    if isinstance(mlp_dims, int): raise ValueError("mlp dimensions should be list, but got int.")
+
+    layers = [nn.Linear(in_dim, mlp_dims[0]), act_fn()]
+    for i in range(len(mlp_dims)-1):
+        layers += [nn.Linear(mlp_dims[i], mlp_dims[i+1]), act_fn()]
+    # the output layer
+    layers += [nn.Linear(mlp_dims[-1], out_dim), out_act()]
+    return nn.Sequential(*layers)
+
+class DQNAgent(object):
+    def __init__(self, state_shape, n_actions,
+                 batch_size=32, hidden_dims=[12], gamma=0.98, lr=1e-3, grad_clip_norm=1000, tau=0.001):
+        self.n_actions = n_actions
+        self.state_dim = state_shape[0]
+
+        self.policy_net = mlp(self.state_dim, hidden_dims, n_actions).to(device)
+        self.target_net = copy.deepcopy(self.policy_net)
+        self.target_net.eval()
+
+        self.optimizer = optim.Adam(self.policy_net.parameters(), lr=lr)
+
+        self.batch_size = batch_size
+        self.gamma = gamma
+        self.grad_clip_norm = grad_clip_norm
+        self.tau = tau
+        
+        self.counter = 0
+
+    def update(self, buffer):
+        """ One gradient step, update the policy net."""
+        self.counter += 1
+        # Do one step gradient update
+        batch = buffer.sample(self.batch_size, device=device)
+        
+        ########## You code starts here #########
+        # Hints: 1. You can use torch.gather() to gather values along an axis specified by dim. 
+        #        2. torch.max returns a namedtuple (values, indices) where values is the maximum 
+        #           value of each row of the input tensor in the given dimension dim.
+        #           And indices is the index location of each maximum value found (argmax).
+        #        3.  batch is a namedtuple, which has state, action, next_state, not_done, reward
+        #           you can access the value be batch.<name>, e.g, batch.state
+        #        4. check torch.nn.utils.clip_grad_norm_() to know how to clip grad norm
+        #        5. You can go throught the PyTorch Tutorial given on MyCourses if you are not familiar with it.
+        
+        # calculate the q(s,a)
+        q_actions_policy = self.policy_net(batch.state)
+        qs = torch.gather(input=q_actions_policy, dim=1, index=batch.action.type(torch.int64))
+
+        # calculate q target 
+        q_actions_target = self.target_net(batch.next_state)
+        max_q = torch.max(input=q_actions_target, dim=1).values
+
+        # set to zero where final state is reached
+        max_q = torch.reshape(max_q, batch.not_done.size())*batch.not_done
+
+        # calculate q target
+        q_tar = batch.reward + self.gamma*max_q
+
+        #calculate the loss 
+        loss = (qs - q_tar.detach())**2 * 0.5
+        loss = loss.sum()
+
+        # clip grad norm and perform the optimization step
+        self.optimizer.zero_grad()
+        loss.backward()
+        torch.nn.utils.clip_grad_norm_(self.policy_net.parameters(), self.grad_clip_norm)
+        self.optimizer.step()
+
+        ########## You code ends here #########
+
+        # update the target network
+        h.soft_update_params(self.policy_net, self.target_net, self.tau)
+        
+        return {'loss': loss.item(), 
+                'q_mean': qs.mean().item(),
+                'num_update': self.counter}
+
+
+    @torch.no_grad()
+    def get_action(self, state, epsilon=0.05):
+        ########## You code starts here #########
+
+        # determine wether to choose greedy policy
+        choose_rand = np.random.random() <= epsilon
+
+        if choose_rand:
+            # take random action
+            return np.random.randint(0, self.n_actions)
+        else:
+            # take greedy action
+            q_actions_target = self.policy_net(torch.from_numpy(state).to(device))
+            return torch.argmax(q_actions_target).item()
+
+
+        ########## You code ends here #########
+
+
+    def save(self, fp):
+        path = fp/'dqn.pt'
+        torch.save({
+            'policy': self.policy_net.state_dict(),
+            'policy_target': self.target_net.state_dict()
+        }, path)
+
+    def load(self, fp):
+        path = fp/'dqn.pt'
+        d = torch.load(path)
+        self.policy_net.load_state_dict(d['policy'])
+        self.target_net.load_state_dict(d['policy_target'])
